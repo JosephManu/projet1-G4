6890293c94cd70b9722f90f5e591a309b86dd8d5
JosephManu <91252020+JosephManu@users.noreply.github.com>
2021-11-10 16:48:24 +0100

    Delete edams directory

--- edams/edams/connexionBDD.php
+++
@@ -1,9 +0,0 @@
-<?php
-    try{
-        $db = new PDO('mysql:host=localhost;dbname=edams','root','');
-        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    
-    }
-    catch(PDOException $e){
-        die('Errer :'.$e->getMessage());
-    }
